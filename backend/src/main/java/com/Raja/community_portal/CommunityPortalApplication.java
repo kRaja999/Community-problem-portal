44d3d09aa3989bf59201d85bf68f291e948b503a
kRaja999 <182rajak@gmail.com>
2026-06-19 19:56:00 +0530

    Add test API endpoint

---
+++ backend/src/main/java/com/Raja/community_portal/CommunityPortalApplication.java
@@ -0,0 +1,13 @@
+package com.Raja.community_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityPortalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Raja/community_portal/CommunityPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Raja.community_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
